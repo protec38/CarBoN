4f1b4f44eb086afe27f4c7f682b848d54469cc09
Rowin <rowin@anhydride-acide.fr>
2024-05-14 00:11:08 +0200

    Refactor defect form logic in its own view

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from main.views import VehicleDetailView, VehicleListView
+from main.views import VehicleDetailView, VehicleListView, DefectCreateView
 
 urlpatterns = [
     path(
-        "vehicles/detail/<int:pk>/",
+        "vehicles/<int:pk>",
         view=VehicleDetailView.as_view(),
         name="vehicle_details",
     ),
-    path("vehicles/", VehicleListView.as_view(), name="vehicles_list"),
+    path("vehicles", VehicleListView.as_view(), name="vehicles_list"),
+    path("vehicles/<int:pk>/defect", DefectCreateView.as_view(), name="defect"),
 ]
