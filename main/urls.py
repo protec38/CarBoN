112d634142f35925645d011adb8dd8fe5130d462
Rowin <rowin@anhydride-acide.fr>
2025-01-07 17:46:46 +0100

    Black

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from main.views import VehicleDetailView, VehicleListView, DefectCreateView, FuelExpenseCreateView, TripStartFormView, \
-    TripEndFormView, TripAbortFormView
+from main.views import (
+    VehicleDetailView,
+    VehicleListView,
+    DefectCreateView,
+    FuelExpenseCreateView,
+    TripStartFormView,
+    TripEndFormView,
+    TripAbortFormView,
+)
 
 urlpatterns = [
     path(
@@ -11,8 +18,16 @@ urlpatterns = [
     ),
     path("vehicles", VehicleListView.as_view(), name="vehicles_list"),
     path("vehicles/<int:pk>/defect", DefectCreateView.as_view(), name="defect"),
-    path("vehicles/<int:pk>/fuel-expense", FuelExpenseCreateView.as_view(), name="fuel_expense"),
-    path("vehicles/<int:pk>/trip/start", TripStartFormView.as_view(), name="trip_start"),
+    path(
+        "vehicles/<int:pk>/fuel-expense",
+        FuelExpenseCreateView.as_view(),
+        name="fuel_expense",
+    ),
+    path(
+        "vehicles/<int:pk>/trip/start", TripStartFormView.as_view(), name="trip_start"
+    ),
     path("vehicles/<int:pk>/trip/end", TripEndFormView.as_view(), name="trip_end"),
-    path("vehicles/<int:pk>/trip/abort", TripAbortFormView.as_view(), name="trip_abort"),
+    path(
+        "vehicles/<int:pk>/trip/abort", TripAbortFormView.as_view(), name="trip_abort"
+    ),
 ]
