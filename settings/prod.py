de6c181179c03a81661c08c014e03a035f489000
rowin <rowin@anhydride-acide.fr>
2025-02-12 00:01:36 +0100

    WIP

--- settings/prod.py
+++ settings/prod.py
@@ -1,6 +1,9 @@
+import os
+
 from settings.settings import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=0))
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
