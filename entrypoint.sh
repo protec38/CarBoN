0e40b2d24063e2d7c4129dcce3c0a2fc4260c1a4
rowin <rowin@anhydride-acide.fr>
2025-02-22 18:42:43 +0100

    Removes timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py migrate --no-input
-poetry run gunicorn -b :8000 settings.wsgi
+poetry run gunicorn -b :8000 -t 0 settings.wsgi
