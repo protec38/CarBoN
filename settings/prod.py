0b0e80b31f6ff865c2a25a19516273f88ba3fe3b
rowin <rowin@anhydride-acide.fr>
2025-02-22 18:19:42 +0100

    Add 0.0.0.0 to ALLOWED_HOSTS

--- settings/prod.py
+++ settings/prod.py
@@ -2,6 +2,6 @@ from settings.settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
 
