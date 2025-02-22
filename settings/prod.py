18480bad7192f96e79704b316b7a653a742fbaf9
rowin <rowin@anhydride-acide.fr>
2025-02-22 18:56:33 +0100

    Trying to fix allowed hosts

--- settings/prod.py
+++ settings/prod.py
@@ -2,6 +2,6 @@ from settings.settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "carbone.apc38.app", "93.127.158.104"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
 
