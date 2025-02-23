a52c6ca20ff5a84bb3b455d4bf37184f742c5d14
rowin <rowin@anhydride-acide.fr>
2025-02-23 15:21:35 +0100

    Adding a script to create the default super user

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py migrate --no-input
-poetry run gunicorn -b :8000 -t 0 settings.wsgi
+poetry run python superuser_creation.py
+poetry run gunicorn -b :8000 settings.wsgi
