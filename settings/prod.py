6c685a73897aae2b6d279bf8cf75ce4a1f000e44
rowin <rowin@anhydride-acide.fr>
2025-02-23 15:21:21 +0100

    Adding nginx back

--- settings/prod.py
+++ settings/prod.py
@@ -2,6 +2,6 @@ from settings.settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "carbone.apc38.app", "93.127.158.104"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(":")
+CSRF_TRUSTED_ORIGINS = [os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS")]
 
