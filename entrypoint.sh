5a2f12985e9514c5edd094b788f2e68c167c7993
Rowin <rowin@anhydride-acide.fr>
2025-06-18 01:15:09 +0200

    Fix docker configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py migrate --no-input
 poetry run python superuser_creation.py
-poetry run gunicorn -b :8000 settings.wsgi
+poetry run gunicorn -b :8003 settings.wsgi
