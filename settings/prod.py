08b29fabe8b6c2b293d9dc2f9779cde6bf28286e
Rowin <rowin@anhydride-acide.fr>
2025-07-30 22:56:47 +0200

    Update prod.py

--- settings/prod.py
+++ settings/prod.py
@@ -5,3 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(":")
 CSRF_TRUSTED_ORIGINS = [os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS")]
 
+print("== PROD SETTINGS ===")
+print(f"ALLOWED HOSTS: {ALLOWED_HOSTS}")
+print(f"CSRF: {CSRF_TRUSTED_ORIGINS}")
+print(f"DEBUG: {DEBUG}")
