01cd72481cd2a77394fbef28ea0e02bd8ec0c2e7
Rowin <rowin@anhydride-acide.fr>
2025-07-31 23:43:30 +0200

    Improve QR code generation

--- main/admin.py
+++ main/admin.py
@@ -72,12 +72,12 @@ class VehicleAdmin(admin.ModelAdmin):
     @admin.action(description=_("Obtenir les QR codes"))
     def get_qr_code(self, request: HttpRequest, queryset: QuerySet):
         context = {
-            "vehicles": {
-                q.name: request.build_absolute_uri(
+            "vehicles": [(
+                q.name, q.registration_number, request.build_absolute_uri(
                     reverse("vehicle_details", args=[q.pk])
-                )
+                ))
                 for q in queryset
-            }
+            ]
         }
         return render(request, "admin/qr_codes.html", context)
 
